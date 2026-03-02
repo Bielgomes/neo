377dcbe96aab39abb7973412f1c8aaac6f4bad95
Bielgomes <bielgomesdasilva@hotmail.com>
2026-03-02 19:24:47 -0300

    rewrite the main.py file and change the file extension to .neo

--- src/main.py
+++ src/main.py
@@ -9,44 +9,52 @@ import sys
 from exceptions.neo_error_base import NeoErrorBase
 
 
+class NeoProgram:
+    @staticmethod
+    def run(source: str) -> None:
+        try:
+            lexer = Lexer(source)
+            tokens = lexer.tokenize()
+
+            parser = Parser(tokens)
+            statements = parser.parse()
+
+            Interpreter().interpret(statements)
+        except NeoErrorBase as err:
+            print(err)
+            return
+
+    @staticmethod
+    def run_file(path: str) -> None:
+        with open(path) as script:
+            source = script.read()
+            NeoProgram.run(source)
+
+    @staticmethod
+    def prompt() -> None:
+        while True:
+            source = input("> ")
+            NeoProgram.run(source)
+
+
 def main() -> None:
     if len(sys.argv) > 2:
         print("Run: python src/main.py [script]")
-    elif len(sys.argv) == 2:
-        if sys.argv[1].split(".")[-1] != "fds":
-            print("Error: File must have a .fds extension.")
-        elif not Path.exists(sys.argv[1]):
-            print(f"Error: File {sys.argv[1]} does not exists.")
-        else:
-            run_file(sys.argv[1])
-    else:
-        prompt()
-
-
-def run(source: str) -> None:
-    try:
-        lexer = Lexer(source)
-        tokens = lexer.tokenize()
-
-        parser = Parser(tokens)
-        statements = parser.parse()
-
-        Interpreter().interpret(statements)
-    except NeoErrorBase as err:
-        print(err)
         return
 
+    if len(sys.argv) == 2:
+        if sys.argv[1].split(".")[-1] != "neo":
+            print("[Error]: File must have a .neo extension.")
+            return
 
-def run_file(path: str) -> None:
-    with open(path) as script:
-        source = script.read()
-        run(source)
+        if not Path.exists(sys.argv[1]):
+            print(f"[Error]: File {sys.argv[1]} does not exists.")
+            return
 
+        NeoProgram.run_file(sys.argv[1])
+        return
 
-def prompt() -> None:
-    while True:
-        source = input("> ")
-        run(source)
+    NeoProgram.prompt()
 
 
 if __name__ == "__main__":
